254312ba2bc7a746398e2d563c3e7a0a8b0169f8
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-12 21:57:07 -0600

    implemented support for printing
    Added support for when the user chooses to print the tree. The tree is currently printed in a pop-up, which I plan to change. Also, fixed a few things to account for the fact that now each function has its own form, as opposed to them all sharing one.

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,6 @@
 import {BST} from "./bst.js";
 
 var tree= BST();  //the binary search tree
-var form=document.getElementById('mainForm'); //the main form on the webpage
 
 //the various buttons:
 var startButton=document.getElementById('start');
@@ -28,11 +27,43 @@ startButton.onclick=function(){
   document.querySelector('h2').textContent="Choose from the following:"; //change initial instruction
   document.getElementById("welcome").style.display="none"; //hide program info
   document.querySelector('ol').style.display="none";
-  document.getElementById('mainForm').style.visibility='visible';
+  document.getElementById('addNode').style.display="block";
+  document.getElementById('removeNode').style.display="block";
+  document.getElementById('printTree').style.display="block";
+  document.getElementById('searchNode').style.display="block";
+  document.getElementById('terminateProgram').style.display="block";
 }
 
 insertButton.onclick=function(){
-  var userInput=form.elements[0].value;
+  var insert=document.getElementById('addNode');
+  var userInput=insert.elements[0].value;
   tree.insert(Number(userInput));
-  form.elements[0].value=""; //clear form
+  insert.elements[0].value=""; //clear form
+}
+
+printButton.onclick=function(){
+  var print=document.getElementById('printTree');
+  var treeContents; //this is a string, returned by the print function
+  if(print.elements[0].checked){
+    //inorder button pressed
+    tree.changePrint("in");
+    //listContents=list.print();
+    tree.print();
+  }
+  else if(print.elements[1].checked){
+    //preorder button pressed
+    tree.changePrint("pre");
+    //listContents=list.print();
+    tree.print();
+  }
+  else if(print.elements[2].checked){
+    //postorder button pressed
+    tree.changePrint("post");
+    //listContents=list.print();
+    tree.print();
+  }
+  else{
+    //user did not press either button
+    alert("please select a direction");
+  }
 }
